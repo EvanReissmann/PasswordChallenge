9f87942298287a39861be134dca75adbffa54868
Evan Reissmann <evanreissmann4@yahoo.com>
2021-11-18 18:18:34 -0500

    added

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ function getRandomInt(min, max) {
 function generatePassword(lowerCase,upperCase,digits,specialCharacters) {
   var passwordLength = getRandomInt( 8, 128);
    console.log(passwordLength);
-  var lowerCase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-  var upperCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-  var digits = ["0,1,2,3,4,5,6,7,8,9"];
-  var specialCharacters = ["!,@,#,$,%,^,&,*,(,)"];
-  
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  var digits = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialCharacters = ["!","@","#","$","%","^","&","*","(",")"];
 }
 
  //Get references to the #generate element
