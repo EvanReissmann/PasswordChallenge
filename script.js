7b2e5dd5dc27e8e8da2f3b9804831fe414560f6c
Evan Reissmann <evanreissmann4@yahoo.com>
2021-11-18 19:03:08 -0500

    finished the java code

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max-min+1)) + min;
 }
-function generatePassword(useLowerCase=false, useUpperCase=false, useDigits=false, useSpecialCharacters=false) {
+function generatePassword(passwordLength, useLowerCase=false, useUpperCase=false, useDigits=false, useSpecialCharacters=false) {
   if (useLowerCase===false && useUpperCase===false && useDigits===false && useSpecialCharacters===false){
     throw "atleast one character type is required";
   }
-  var passwordLength = getRandomInt( 8, 128);
-   console.log(passwordLength);
   var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var digits = ["0","1","2","3","4","5","6","7","8","9"];
@@ -26,7 +24,7 @@ function generatePassword(useLowerCase=false, useUpperCase=false, useDigits=fals
   if (useSpecialCharacters===true){
     allowed=allowed.concat(specialCharacters);
   }
-  for (var i=0; i<=passwordLength; i++){
+  for (var i=0; i<passwordLength; i++){
     var element=getRandomInt(0,allowed.length-1);
     password += allowed[element];
   }
@@ -38,8 +36,12 @@ var generateBtn = document.querySelector("#generate");
 
  //Write password to the #password input
 function writePassword() {
-  var password = generatePassword(useLowerCase=true,useUpperCase=true,useDigits=true,useSpecialCharacters=true);
-  console.log(password)
+  var passwordLength=window.prompt("select a number between 8 and 128");
+  var useLowerCase=window.confirm("use lowercase letters");
+  var useUpperCase=window.confirm("use uppercase letters");
+  var useDigits=window.confirm("use digits");
+  var useSpecialCharacters=window.confirm("use special characters");
+  var password = generatePassword(passwordLength=passwordLength,useLowerCase=useLowerCase,useUpperCase=useUpperCase,useDigits=useDigits,useSpecialCharacters=useSpecialCharacters);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
